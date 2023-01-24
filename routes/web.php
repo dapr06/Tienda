2d9780448d936dc989b554352df4f0f391a35bd0
pruebadanielaruben <rjimenez11@educa.madrid.org>
2023-01-24 12:58:14 +0100

    LOGIN RUBEN

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductoController;
-use App\Http\Controllers\ProveedorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('productos', ProductoController::class);
-Route::resource('proveedores', ProveedorController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::resource('productos',\App\Http\Controllers\ProductoController::class);
+Route::resource('proveedores',\App\Http\Controllers\ProveedorController::class);
