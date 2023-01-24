4848456735061e48c59715f0078ab1a72b7df2f0
dapr06 <danielaprato12@gmail.com>
2023-01-24 13:08:45 +0100

    verificacion daniela

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -2,7 +2,7 @@
 
     <x-slot:tituloHead>Crear producto</x-slot:tituloHead>
 
-   
+
     <form action='{{ route('productos.store') }}' method='post'>
         @method('post')
         @csrf
@@ -12,6 +12,15 @@
         <input class='button' type='submit' name='crear' value='Crear producto' />
     </form><br/>
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <a href='{{ route('productos.index') }}'>Volver al listado</a>
 
 </x-base>
