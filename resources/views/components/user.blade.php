7432815747f9055d05dd0cd6b836e551a476ac0d
pruebadanielaruben <rjimenez11@educa.madrid.org>
2023-02-16 13:57:57 +0100

    cambios rol 16/2/23 ordenador ruben

--- resources/views/components/user.blade.php
+++ resources/views/components/user.blade.php
@@ -1,5 +1,20 @@
 @if(Auth::check())
-    <p>Usuario: {{ Auth::user()->name}}
+    <p>Usuario: {{ Auth::user()->name }}
+
+    <?php
+
+    $user=Auth::user();
+
+if($user->esAdmin()){
+    echo "eres admin";
+}else{
+    echo "eres cliente";
+}
+?>
+
+
+
+
     <form method="POST" action="{{ route('logout') }}">
         @csrf
         <button type="submit">Logout</button>
